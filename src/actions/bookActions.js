a785e7878cdd77b1c8538c9fd01173501076bfbc
Ganesh-Matkar <gmatkar0@gmail.com>
2024-11-16 18:12:45 +0530

    Backend URL change

--- src/actions/bookActions.js
+++ src/actions/bookActions.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const fetchBooks = (query = {}) => async (dispatch) => {
     try {
         dispatch({ type: 'BOOKS_REQUEST' });
-        const { data } = await axios.get('http://localhost:4000/books', { params: query });
+        const { data } = await axios.get('https://book-library-server-ecru.vercel.app/books', { params: query });
         dispatch({ type: 'BOOKS_SUCCESS', payload: data });
     } catch (error) {
         dispatch({ type: 'BOOKS_FAIL', payload: error.message });
